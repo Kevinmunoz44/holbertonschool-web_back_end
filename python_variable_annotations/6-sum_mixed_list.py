eda857d8df39fe259f98bf6bedecf34f25d22191
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-08-29 11:12:21 -0500

    Task fix 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Complex types - list of floats """
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int]) -> float:
+    """
+    function sum_list which takes a list input_list of
+    floats as argument and returns their sum as a float.
+    """
+    return sum(mxd_lst)
