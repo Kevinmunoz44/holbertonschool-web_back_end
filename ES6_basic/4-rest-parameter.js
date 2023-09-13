8c487d93e4375f2d8c730c3edde6f7a4e5cf46f9
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-09-13 11:31:12 -0500

    Update file and fix 4

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,7 +1,7 @@
 export default function returnHowManyArguments(...args) {
     let total = 0;
     for(let arg of args) {
-        total += arg.length
+        total += arg
     }
-    return total;
+    return total.length();
 }
